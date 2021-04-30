fec3d71941fcaa140acf4e3436d1107fabe7a08d
Péter Kalicz <kaliczp@gmail.com>
2021-04-30 19:49:18 +0200

    Double SD widht around line, pdf, margin
    Standard deviation band doubled around thetas.
    
    pdf generation tested.
    
    Margin set changed from default.
    
    yaxs internal.

--- TradPlot.R
+++ TradPlot.R
@@ -1,5 +1,7 @@
-par(las = 1)
-plot.zoo(Thetasum[,"K7ÉPth"], lwd=2, xaxs = "i", ylab = "Theta [%]", ylim = c(0,20), type="n", xaxt = "n", xlab = "")
+pdf(width=14)
+par(mar=c(5.1,4.1,2.1,4.1), las = 1)
+plot.zoo(Thetasum[,"K7ÉPth"], lwd=2, xaxs = "i", ylab = "Theta [%]",
+         ylim = c(0,20), yaxs ="i", type="n", xaxt = "n", xlab = "")
 grid(nx = NA, ny = NULL)
 tick.month <- seq.Date(as.Date("2018-08-01"), as.Date("2021-04-01"), by = "1 month")
 axis(1, at = tick.month, tck = 1,
@@ -7,12 +9,12 @@ axis(1, at = tick.month, tck = 1,
 axis(1, at = tick.month, lab=F)
 axis.Date(1, at = tick.month + 15, tcl = 0, format = "%m")
 lines(as.zoo(Thetasum[,"K7ÉPth"]), lwd = 2)
-lines(as.zoo(Thetasum[,"K7ÉPth"] - Thetasum[,"K7ÉPsd"]), lty="dotted")
-lines(as.zoo(Thetasum[,"K7ÉPth"] + Thetasum[,"K7ÉPsd"]), lty="dotted")
+lines(as.zoo(Thetasum[,"K7ÉPth"] - 2 * Thetasum[,"K7ÉPsd"]), lty="dotted")
+lines(as.zoo(Thetasum[,"K7ÉPth"] + 2 * Thetasum[,"K7ÉPsd"]), lty="dotted")
 lines(as.zoo(Thetasum[,"K35Fth"]), lwd = 2, col = 2)
-lines(as.zoo(Thetasum[,"K35Fth"] - Thetasum[,"K35Fsd"]), lty="dotted", col = 2)
-lines(as.zoo(Thetasum[,"K35Fth"] + Thetasum[,"K35Fsd"]), lty="dotted", col = 2)
+lines(as.zoo(Thetasum[,"K35Fth"] - 2 * Thetasum[,"K35Fsd"]), lty="dotted", col = 2)
+lines(as.zoo(Thetasum[,"K35Fth"] + 2 * Thetasum[,"K35Fsd"]), lty="dotted", col = 2)
 lines(as.zoo(Thetasum[,"K12Eth"]), lwd = 2, col = 3)
-lines(as.zoo(Thetasum[,"K12Eth"] - Thetasum[,"K12Esd"]), lty="dotted", col = 3)
-lines(as.zoo(Thetasum[,"K12Eth"] + Thetasum[,"K12Esd"]), lty="dotted", col = 3)
+lines(as.zoo(Thetasum[,"K12Eth"] - 2 * Thetasum[,"K12Esd"]), lty="dotted", col = 3)
+lines(as.zoo(Thetasum[,"K12Eth"] + 2 * Thetasum[,"K12Esd"]), lty="dotted", col = 3)
 
