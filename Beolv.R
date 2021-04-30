a5500aa252b6f6ad306c0a0d45a0bba7fce4e635
Péter Kalicz <kaliczp@gmail.com>
2021-04-30 14:53:37 +0200

    Read all measurements instead mean
    All soilmoisture measurements are read into one data.frame and converted to
    numeric.

--- Beolv.R
+++ Beolv.R
@@ -1,8 +1,12 @@
 ## Excel
 library(readxl)
 
+## Munka 2
 nyers <- read_xlsx("Mérések_Kecskemét.xlsx","Munka2")
 
-## Munka2
-Thetamean <- as.data.frame(nyers)[-c(1:2),c(1:2,8,14)]
-names(Thetamean) <- c("Date", "K7ÉP", "K35F", "K12E")
+Thetaall <- as.data.frame(nyers)[-c(1:2),-c(2,7:8,13:14,19)]
+names(Thetaall) <- c("Date", paste0("K7ÉP",1:4), paste0("K35F",1:4), paste0("K12E",1:4))
+
+for(tti in 2:ncol(Thetaall)) {
+Thetaall[,tti] <- as.numeric(Thetaall[,tti])
+}
