f0533b103481cb589359d1a07eb1d1a534409cf0
Péter Kalicz <kaliczp@gmail.com>
2021-04-30 16:06:13 +0200

    Time-series test
    First point data generated and ploted.

--- Beolv.R
+++ Beolv.R
@@ -10,3 +10,12 @@ names(Thetaall) <- c("Date", paste0("K7ÉP",1:4), paste0("K35F",1:4), paste0("K1
 for(tti in 2:ncol(Thetaall)) {
 Thetaall[,tti] <- as.numeric(Thetaall[,tti])
 }
+
+library(xts)
+K7ÉP <- xts(data.frame(theta1 = rowMeans(Thetaall[,2:5]),
+                       sd = apply(Thetaall[,2:5], 1, sd)),
+            as.Date(Thetaall[,1])
+            )
+plot.zoo(K7ÉP[,1], lwd=2, xaxs = "i", ylab = "Theta [%]", ylim = c(0,20))
+lines(as.zoo(K7ÉP[,"theta1"] - K7ÉP[,"sd"]), lty="dotted")
+lines(as.zoo(K7ÉP[,"theta1"] + K7ÉP[,"sd"]), lty="dotted")
