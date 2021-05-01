faafccd00dd3680a0a88ff67b08247bff1b4a3bc
Péter Kalicz <kaliczp@gmail.com>
2021-05-01 12:32:09 +0200

    Visual enhancements
    Bottom axis width is decreased. Missing month name is generated. Years are separated and explained. Precipitation colums are wider.

--- TradPlot.R
+++ TradPlot.R
@@ -1,15 +1,20 @@
 pdf(width=14)
 orilocale <- Sys.getlocale("LC_TIME")
 Sys.setlocale("LC_TIME", "C")
-par(mar=c(5.1,4.1,2.1,4.1), las = 1)
+par(mar=c(3.1,4.1,2.1,4.1), las = 1)
 plot.zoo(Thetasum[,"K7ÉPth"], lwd=2, xaxs = "i", ylab = "Theta [%]",
          ylim = c(0,20), yaxs ="i", type="n", xaxt = "n", xlab = "")
 grid(nx = NA, ny = NULL)
-tick.month <- seq.Date(as.Date("2018-08-01"), as.Date("2021-04-01"), by = "1 month")
+tick.month <- seq.Date(as.Date("2018-07-01"), as.Date("2021-04-01"), by = "1 month")
+tick.year <- seq.Date(as.Date("2019-01-01"), as.Date("2021-01-01"), by = "1 year")
 axis(1, at = tick.month, tck = 1,
      col = "lightgray", lty = "dotted", lab = F)
-axis(1, at = tick.month, lab=F)
-axis.Date(1, at = tick.month + 15, tcl = 0, format = "%m")
+axis(1, at = tick.year, tck = 1,
+     col = "lightgray", lab = F, lwd = 2)
+axis(1, at = tick.month[-1], lab=F)
+axis.Date(1, at = tick.month + 15, tcl = 0, format = "%b", cex.axis = 0.75)
+yeartext <- c(tick.year - c(91,183,183), tick.year[length(tick.year)] + 50)
+text(x = yeartext, y = rep(0.8, length(tick.year)), labels = format(yeartext, format = "%Y"), cex = 2, col = "lightgray")
 lines(as.zoo(Thetasum[,"K7ÉPth"]), lwd = 2)
 lines(as.zoo(Thetasum[,"K7ÉPth"] - 2 * Thetasum[,"K7ÉPsd"]), lty="dotted")
 lines(as.zoo(Thetasum[,"K7ÉPth"] + 2 * Thetasum[,"K7ÉPsd"]), lty="dotted")
@@ -23,7 +28,7 @@ par(new = TRUE)
 plot.zoo(Thetasum[,"K7ÉPth"], lwd=2, xaxs = "i",
          ylim = c(-200,0), yaxs ="i", type="n", axes = FALSE, xlab = "",
          ylab = "")
-lines(as.zoo(All.xts$Prec * -1), type = "h", col = "blue", lend = 1, lwd = 2)
+lines(as.zoo(All.xts$Prec * -1), type = "h", col = "blue", lend = 1, lwd = 4)
 axis(4, at=c(0,-50,-100), lab = c(0,50,100))
 mtext("Precipitation [mm]", side = 4, line = 3, at = -25, las = 0)
 Sys.setlocale("LC_TIME", orilocale)
