92f53a9de8ad54772fe47c4bb4be3c3475235de3
Péter Kalicz <kaliczp@gmail.com>
2021-05-01 19:35:07 +0200

    Fig 2 two GW time-series plotted
    Two plots are overplotted with matching axes.

---
+++ Fig2plot.R
@@ -0,0 +1,13 @@
+par(mar=c(3.1,4.1,2.1,4.1), las = 1)
+plot.zoo(talaj1.xts$GW['/2020-02-12'], lwd = 2, xaxs = "i",
+         xlim = c(as.Date("2018-07-03"), as.Date("2020-02-12")),
+         ylim = c(-9.1, -7.1), yaxt = "n", ylab = "")
+axis(2, at = c(-8, -8.5, -9))
+mtext("GW [m below surface]", side = 2, line = 3, at = -8.5, las = 0)
+grid(nx = NA, ny = NULL)
+par(new = TRUE)
+plot.zoo(talaj2.xts$GW, lwd = 2, xaxs = "i",
+         xlim = c(as.Date("2018-07-03"), as.Date("2020-02-12")),
+         ylim = c(-5.1, -3.1), yaxt = "n", ylab ="", col = 2)
+mtext("GW VIZIG [m below surface]", side = 4, line = 3, at = -4, las = 0, col = 2)
+axis(4, at = c(-3.5, -4, -4.5), col = 2, col.tick = 2)
