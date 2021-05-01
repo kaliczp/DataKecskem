83c895e49ef91ef97bb3a8619fed64f2ce701116
Péter Kalicz <kaliczp@gmail.com>
2021-05-01 20:42:40 +0200

    Precipitation plotted
    Precipitation added to the plot. Y-axis matches with GW axes.

--- Fig2plot.R
+++ Fig2plot.R
@@ -11,3 +11,11 @@ plot.zoo(talaj2.xts$GW, lwd = 2, xaxs = "i", yaxs = "i",
          ylim = c(-5.2, -3.0), yaxt = "n", ylab ="", col = 2)
 mtext("GW VIZIG [m below surface]", side = 4, line = 3, at = -4, las = 0, col = 2)
 axis(4, at = c(-3.5, -4, -4.5), col = 2, col.tick = 2)
+par(new = TRUE)
+plot.zoo(talaj1.xts['/2020-02-12',"Prec"], xaxs = "i", yaxs = "i", type = "n",
+         xlim = c(as.Date("2018-07-03"), as.Date("2020-02-12")),
+         ylim = c(220, 0), yaxt = "n", ylab ="")
+axis(2, at = 25, tck = 1, col = "lightgray", lty = "dotted", lab = F)
+lines(as.zoo(talaj1.xts['/2020-02-12',"Prec"]), type = "h", col = "blue", lend = 1, lwd = 3)
+axis(2, at = c(0, 25, 50), col.tick = "blue")
+mtext("Precipitation [mm]", side = 2, line = 3, at = 25, las = 0, col = "blue")
