68c888b41250a48839f3d8677ea1fec071d490ff
Péter Kalicz <kaliczp@gmail.com>
2022-04-12 12:58:02 +0200

    More compact format is created
    Wider figure with narrower marigns.
    Precipitation axis is modified accordingly to soil moisture scale.

--- Fig6Moist10cm.R
+++ Fig6Moist10cm.R
@@ -6,9 +6,9 @@ Sys.setenv(TZ='UTC')
 orilocale <- Sys.getlocale("LC_TIME")
 Sys.setlocale("LC_TIME", "C")
 tick.day <- seq.POSIXt(as.POSIXct("2021-05-12 00:00"), as.POSIXct("2021-06-17 00:00"), by = "1 day")
-par(mar=c(3.1,4.1,2.1,4.1), las = 2)
-plot.zoo(Abr10cm.xts[,1], type = "n",
-         xlim = tick.day[c(1, length(tick.day))], ylim = c(0,55), yaxs = "i", xaxs = "i", xaxt = "n",
+par(mar=c(3.1,3.1,1.1,3.2), las = 2, mgp=c(2.2,1,0))
+plot.zoo(Abr10cmh.xts[,1], type = "n",
+         xlim = tick.day[c(1, length(tick.day))], ylim = c(0,30), yaxs = "i", xaxs = "i", xaxt = "n",
          xlab = "", ylab = "Soil moisture [%]")
 grid(nx = NA, ny = NULL)
 axis(1, at = tick.day, tck = 1,
@@ -21,14 +21,14 @@ axis.POSIXct(1, at = tick.day + 12 * 60 * 60, tcl = 0, format = "%d", cex.axis =
 par(mgp = c(3, 2, 0))
 axis.POSIXct(1, at = as.POSIXct(c("2021-05-21 00:00", "2021-06-10 00:00")), tcl = 0, format = "%b", cex.axis = 0.8, las = 1)
 par(mgp = c(3, 1, 0))
-lines(as.zoo(Abr10cm.xts[,1]), lwd =2, col = 3)
-lines(as.zoo(Abr10cm.xts[,2]), lwd =2, col = 1)
-lines(as.zoo(Abr10cm.xts[,3]), lwd =2, col = 2)
+lines(as.zoo(Abr10cmh.xts[,1]), lwd =2, col = 3)
+lines(as.zoo(Abr10cmh.xts[,2]), lwd =2, col = 1)
+lines(as.zoo(Abr10cmh.xts[,3]), lwd =2, col = 2)
 par(new = TRUE)
-plot(as.zoo(AbrCsapd.xts), type = "h", col = "lightblue", ylim = c(200,0),
+plot(as.zoo(AbrCsapd.xts), type = "h", col = "lightblue", ylim = c(150,0),
         yaxs = "i", xaxs = "i", xaxt = "n", yaxt = "n", lwd = 15, lend = 2,
         xlim = tick.day[c(1, length(tick.day))], xlab = "", ylab = "")
 axis(4, at = c(0,25,50))
-mtext("Precipitation [mm/day]", side = 4, line = 3, at = 25, las = 0)
+mtext("Precipitation [mm/day]", side = 4, line = 2.2, at = 25, las = 0)
 box()
-legend(as.POSIXct("2021-06-01 00:00"), 100, legend = c("Black Locust", "Black Pine", "Control"), col = c(1:3, "blue"), lty=1, lwd = c(2, 2, 2) , bg = "white")
+legend(as.POSIXct("2021-06-01 00:00"), 50, legend = c("Black Locust", "Black Pine", "Control"), col = c(1:3, "blue"), lty=1, lwd = c(2, 2, 2) , bg = "white")
